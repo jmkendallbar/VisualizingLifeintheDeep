9641860b10e52e2b6180add47f7cdae6d8c45c89
nick <nxkbar@gmail.com>
2021-05-15 11:43:10 -0700

    seal sampling script: add handling for stroke and glide data

--- python/Seal Sample Data Input Code.py
+++ python/Seal Sample Data Input Code.py
@@ -9,19 +9,26 @@ HEAD_DEG = 3
 X_POS = 4
 Z_POS = 5
 DEPTH = 6
+GLIDE = 9
+STROKE = 11
 
 # Defining two variables which will be used as indices where animation starts and ends
-START = 0  # start time in sec
-END = 12  # end time in sec
+START = 48  # start time in sec
+END = 65  # end time in sec
 
-fs = 10  # Sample frequency (in Hz or "samples per second")
+fs = 16  # Sample frequency (in Hz or "samples per second")
+
 
 # Reading in .csv file (update to reflect your own path)
-with open('C:/Users/jmkb9/Pictures/Art/Workshops/Sample Seal Data.csv') as csv_file:
+with open('{DIR_PATH}/VisualizingFear/data/Glacier_stroke_analysis.csv') as csv_file:
     data = csv.reader(csv_file, delimiter=',')
+    headers = next(data)
+    print('headers: %s' % headers)
 
     # For loop runs through all rows in data .csv file
-    for i, row in enumerate(data):
+    for i, row in enumerate(data, 1):
+        i -= 1 # correct for header row
+        
         if i % 10000 == 0:
             print 'Processing row %s' % (i)
             # Print progress in with data row counter in console to keep track of if/where code gets stuck
@@ -30,15 +37,25 @@ with open('C:/Users/jmkb9/Pictures/Art/Workshops/Sample Seal Data.csv') as csv_f
         if i >= START * fs and i < END * fs:
             # We will use the function float() to return floating point numbers (with decimals) for data values
 
-            time = float(i) / fs - START  # Translate .csv data time into animation time
-            time = time * 24  # Get from frames to seconds
+            clock_time = float(i) / fs - START  # Translate .csv data time into animation time
+            time = clock_time * 24 + 1  # Get from seconds to frame
+            print('row', i, 'time', clock_time, 'frame', time)
 
-            translateX_value = float(row[X_POS])
-            translateZ_value = -float(row[Z_POS])  # to fit axis orientations
-            depth_value = float(row[DEPTH])
+            # translateX_value = float(row[X_POS])
+            # translateZ_value = -float(row[Z_POS])  # to fit axis orientations
+            # depth_value = float(row[DEPTH])
 
             # Define which object will be transformed according to data (use name as described in "Outliner")
-            object = pm.ls('elephantseal')[0]
+            object = pm.ls('SWIM_CONTROL')[0]
+            
+            glide = float(row[GLIDE])
+            object.glide.setKey(value=glide, time=time)
+            
+            swim_stroke = int(row[STROKE])
+            if swim_stroke:
+                object.swimY.setKey(value=0, time=time)
+                object.swimY.setKey(value=1, time=time - .001)
+                pm.keyTangent(object.swimY, inTangentType='linear', outTangentType='linear', time=(time - .001, time))
 
             # ..setKey function sets a keyframe of the given value at the given time.
             object.translateX.setKey(value=translateX_value, time=time)
@@ -55,4 +72,5 @@ with open('C:/Users/jmkb9/Pictures/Art/Workshops/Sample Seal Data.csv') as csv_f
             object.rotateZ.setKey(value=roll_value, time=time)
 
             print 'setting y= %s msw, x= %s, z= %s, rotX= %s, rotY= %s, rotZ = %s for time= %s frames' % (
-            depth_value, translateX_value, translateZ_value, pitch_value, head_value, roll_value, time)
+                depth_value, translateX_value, translateZ_value, pitch_value, head_value, roll_value, time)
+
